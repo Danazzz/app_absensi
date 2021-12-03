70b1c05f63bfc20527c50eb5ea00fd641a3e7ad4
Danazzz <artaputra95@gmail.com>
2021-12-03 16:55:12 +0800

    menambah unlink untuk menghapus gambar

--- admin/del_mahasiswa.php
+++ admin/del_mahasiswa.php
@@ -1,5 +1,17 @@
 <?php
 require_once"../_config/config.php";
 
+$id = @$_GET['id'];
+$sql = "SELECT * FROM tb_user
+INNER JOIN tb_mahasiswa ON tb_user.id_user = tb_mahasiswa.id_user
+WHERE id_mahasiswa='$id'
+";
+$query = mysqli_query($con, $sql);
+$data = mysqli_fetch_array($query);
+$gambar= $data['gambar'];
+$path = "../_assets/uploads/".$gambar;
+unlink($path);
+
 mysqli_query($con, "DELETE FROM tb_mahasiswa WHERE id_mahasiswa = '$_GET[id]'") or die(mysqli_error($con));
 echo "<script>alert('Absensi telah dihapus!');window.location='data_mahasiswa.php';</script>";
+?>
